1ab6210ae21a9e43c8671b828a67c45578f1dfe1
Farid83300 <farid.zaffalone@gmail.com>
2026-02-18 22:08:39 +0100

    Création de la class Contact

---
+++ contact.php
@@ -0,0 +1,78 @@
+<?php
+
+class Contact
+{
+    private ?int $id;
+    private ?string $name;
+    private ?string $email;
+    private ?string $phoneNumber;
+
+    public function __construct(
+        ?int $id = null,
+        ?string $name = null,
+        ?string $email = null,
+        ?string $phoneNumber = null
+    ) {
+        $this->id          = $id;
+        $this->name        = $name;
+        $this->email       = $email;
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    // --- Getters ---
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    // --- Setters ---
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setPhoneNumber(?string $phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    // --- Représentation textuelle ---
+
+    public function toString(): string
+    {
+        return sprintf(
+            "[%d] %s | %s | %s",
+            $this->id          ?? 0,
+            $this->name        ?? 'N/A',
+            $this->email       ?? 'N/A',
+            $this->phoneNumber ?? 'N/A'
+        );
+    }
+}
